10c21c92e509831e77ce55b7451090a91f01fd81
tal bechor <tal.bar.bechor@gmail.com>
2022-02-10 19:03:17 +0200

    .

---
+++ src/main/java/dbManager/DBManager.java
@@ -0,0 +1,7 @@
+package dbManager;
+
+public class DBManager {
+    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String SQL_USER = "root";
+    public static final String SQL_PASS = "12345678";
+}
